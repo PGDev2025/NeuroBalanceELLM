8387250c47cf0693ab5e974fcc1a0fb56943a205
PGDev2025 <240103047@hbtu.ac.in>
2025-11-14 15:18:17 +0530

    Add files via upload

---
+++ neuro_balance_predictor.py
@@ -0,0 +1,15 @@
+import joblib
+import numpy as np
+
+model = joblib.load("neuro_balance_model.pkl")
+scaler = joblib.load("neuro_balance_scaler.pkl")
+
+def predict_mfi(sentiment_score, activity_score, rest_balance):
+    input_df = pd.DataFrame({
+        "sentiment_score": [sentiment_score],
+        "activity_score": [activity_score],
+        "rest_balance": [rest_balance]
+    })
+    input_scaled = scaler.transform(input_df)
+    prediction = model.predict(input_scaled)[0]
+    return float(prediction)
